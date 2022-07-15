7640019c7fa0bcb22c44a27723ed432063a729e4
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-07-15 19:05:10 -0300

    feat: adiciona requisito 07
    Cria o modelo 'Category' em 'src/database/models/category.js'
    seguindo o diagrama de ER

---
+++ src/database/models/category.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+
+const attributes = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+};
+
+/** @param {import('sequelize').Sequelize} sequelize */
+module.exports = (sequelize) => {
+  const categories = sequelize.define('Category', attributes, { tableName: 'Categories' , timestamps: false});
+  return categories;
+};
